3ad81f7be84067471d829be45963d06ab9695263
Mohammed Ahmed <reazssj4@gmail.com>
2021-10-28 15:10:27 +0100

    Updated

--- Data/Migrations/20211028135338_Required.Designer.cs
+++ Data/Migrations/20211028135338_Required.Designer.cs
@@ -4,14 +4,16 @@ using MVC_Movies.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MVC_Movies.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211028135338_Required")]
+    partial class Required
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Models/Movie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Movies.Models
+{
+    public enum Genres
+    {
+        Comedy,
+        Drama,
+        Thriller,
+        Action,
+        Horror
+    }
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(60), Required]
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public Genres Genre { get; set; } = Genres.Action;
+
+        [Range(5.99, 7.99), Required]
+        public decimal Price { get; set; }
+    }
+}
